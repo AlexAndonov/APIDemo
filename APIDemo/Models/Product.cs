df13a3167ab8de8f6216ad24cc624c0d713d7c62
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-01-16 10:26:26 +0200

    Adding DataAnnotations to Product

--- APIDemo/Models/Product.cs
+++ APIDemo/Models/Product.cs
@@ -1,9 +1,19 @@
-﻿namespace APIDemo.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIDemo.Models
 {
 	public class Product
 	{
+		[Key]
 		public int Id { get; set; }
+
+		[Required]
+		[MaxLength(100)]
 		public string Name { get; set; } = string.Empty;
+
+		[Required]
+		[Column(TypeName = "decimal(18,2)")]
 		public decimal Price { get; set; }
 	}
 }
