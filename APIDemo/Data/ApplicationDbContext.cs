5b56dd1e5619835857c79077203583151406cf0e
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-01-16 10:25:59 +0200

    Adding ApplicationDbContext class

---
+++ APIDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APIDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base (options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
